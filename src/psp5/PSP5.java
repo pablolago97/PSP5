0b5b62b5b777d80fd7cb2dfafca8e18bac31ff3a
Pablo <Pablo@192.168.0.195>
2016-11-06 10:54:50 +0100

    Terminado

--- src/psp5/PSP5.java
+++ src/psp5/PSP5.java
@@ -1,6 +1,9 @@
 
 package psp5;
 
+import static java.lang.Thread.MAX_PRIORITY;
+import static java.lang.Thread.MIN_PRIORITY;
+
 
 public class PSP5 {
 
@@ -8,9 +11,9 @@ public class PSP5 {
     public static void main(String[] args) {
         //Declaro los hilos y ordeno sus prioridades
         Thread hilo1 = new Orden ("hilo1");
-        hilo1.setPriority(1);
+        hilo1.setPriority(MIN_PRIORITY);
         Thread hilo2 = new Orden("hilo2");
-        hilo2.setPriority(10);
+        hilo2.setPriority(MAX_PRIORITY);
         //Inicializo los hilos
         hilo1.start();
         hilo2.start();
